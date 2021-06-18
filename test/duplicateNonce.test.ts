d990ce0d8e3f366500404b25ee5a219d063ce158
Jesse Clark <hello@developerjesse.com>
2021-06-18 12:58:30 +0300

    Add second tx using the rejectTx method.

--- test/duplicateNonce.test.ts
+++ test/duplicateNonce.test.ts
@@ -1,11 +1,12 @@
 import EthersSafe from '@gnosis.pm/safe-core-sdk'
 import { expect } from 'chai'
 import { deployments, ethers, waffle } from 'hardhat'
+import { rejectTx } from '../src'
 import { EMPTY_DATA } from '../src/utils/constants'
 import { getSafeWithOwners } from './utils/setup'
 
 describe('checking nonce', () => {
-  const [user1] = waffle.provider.getWallets()
+  const [user1, user2] = waffle.provider.getWallets()
 
   const setupTests = deployments.createFixture(async ({ deployments }) => {
     await deployments.fixture()
@@ -31,13 +32,17 @@ describe('checking nonce', () => {
     await ethersSafe.signTransactionHash(tx1Hash)
 
     // create a second transaction with the same nonce:
+    /*
     const tx2 = await ethersSafe.createTransaction({
-      to: user1.address,
+      to: user2.address,
       value: '0',
       data: EMPTY_DATA
     })
+    */
+    // OR... create a second transaction using the rejectTx method:
+    const tx2 = await rejectTx(ethersSafe, tx1)
 
-    // SafeTransaction nonce is the same:
+    // The nonceS are the same:
     expect(tx2.data.nonce).to.equal(tx1.data.nonce)
 
     // sign the second transaction:
